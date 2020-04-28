4ab50b75918ad62829b59aab83b6168c33c02e41
Tavishi Singh <tavishisinghx@gmail.com>
2020-04-29 01:19:00 +0530

    Final Commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,5 +4,20 @@
  */
 package main;
 
+import myqueue.MyQueueUsingArray;
+
 public class Main {
+    public static void main(String[] args) {
+        MyQueueUsingArray queue=new MyQueueUsingArray(10);
+        queue.enqueue(1000);
+        System.out.println(queue.getSize());
+        System.out.println(queue.peek() + " element at front");
+        queue.enqueue(300);
+        queue.enqueue(400);
+        queue.enqueue(200);
+        System.out.println(queue.dequeue() + " element deleted from queue");
+        System.out.println("The size queue is: " + queue.getSize());
+        System.out.println(queue.peek());
+
+    }
 }
